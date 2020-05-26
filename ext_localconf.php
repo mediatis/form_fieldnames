d2203869c5c9ec432f321d44bb82d9cf57cf061e
Stephan Ude <ude@mediatis.de>
2020-05-26 17:52:34 +0200

    [BUGFIX] fixed extension key

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
     $typoscript = 'module.tx_form.settings.yamlConfigurations {
-  1522324494 = EXT:formrelay/Configuration/Yaml/FormEditorSetup.yaml
+  1522324494 = EXT:form_fieldnames/Configuration/Yaml/FormEditorSetup.yaml
 }';
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup($typoscript);
 });
