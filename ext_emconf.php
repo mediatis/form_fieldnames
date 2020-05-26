85bf065e8c2f6e5608de3c73c9c52d421b647557
Stephan Ude <ude@mediatis.de>
2020-05-26 11:49:27 +0200

    [TASK] initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,24 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Form Field Names',
+    'description' => 'Extends ext:form by adding the field "name" to all form fields.',
+    'category' => 'be',
+    'author_email' => 'info@mediatis.de',
+    'author_company' => 'Mediatis AG',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '1',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-10.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
