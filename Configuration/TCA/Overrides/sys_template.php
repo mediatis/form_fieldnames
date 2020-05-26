e9e93c8d4d07ac1823140d8e18f8b49aaaff07a6
Stephan Ude <ude@mediatis.de>
2020-05-26 13:44:00 +0200

    [FEATURE] typoscript is now always loaded

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-defined('TYPO3_MODE') or die();
-
-call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('form_fieldnames', 'Configuration/TypoScript', 'Form Field Names');
-});
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,13 +2,20 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-        'module.tx_form {
+    $typoscript = 'module.tx_form {
     settings {
         yamlConfigurations {
             1522324493 = EXT:form_fieldnames/Configuration/Yaml/FormSetup.yaml
         }
     }
-}'
-    );
+}
+
+plugin.tx_form {
+    settings {
+        yamlConfigurations {
+            1522324493 = EXT:form_fieldnames/Configuration/Yaml/FormSetup.yaml
+        }
+    }
+}';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup($typoscript);
 });
