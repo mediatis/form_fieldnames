251811d71e5cb81e0a28dee2f92a0b38c9c3a872
Stephan Ude <ude@mediatis.de>
2020-05-26 15:33:08 +0200

    [TASK] compatibility with TYPO3 v9

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,20 +2,8 @@
 defined('TYPO3_MODE') or die();
 
 call_user_func(function () {
-    $typoscript = 'module.tx_form {
-    settings {
-        yamlConfigurations {
-            1522324493 = EXT:form_fieldnames/Configuration/Yaml/FormSetup.yaml
-        }
-    }
-}
-
-plugin.tx_form {
-    settings {
-        yamlConfigurations {
-            1522324493 = EXT:form_fieldnames/Configuration/Yaml/FormSetup.yaml
-        }
-    }
+    $typoscript = 'module.tx_form.settings.yamlConfigurations {
+  1522324494 = EXT:formrelay/Configuration/Yaml/FormEditorSetup.yaml
 }';
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup($typoscript);
 });
