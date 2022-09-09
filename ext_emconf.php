0754cdcc15eceb7116d8939b52212074f76bea11
Michael Vöhringer <voehringer@mediatis.de>
2022-09-09 13:46:37 +0200

    [BUGFIX] Remove php7.x dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,4 +1,5 @@
 <?php
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Form Field Names',
     'description' => 'Extends ext:form by adding the field "name" to all form fields.',
@@ -6,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '3.4.0',
+    'version' => '3.5.0',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
