e6d7f0b02fd1ac1cb86f269b2357ec89fab4c21e
Meelis Karulin <karulin@mediatis.de>
2025-02-10 14:24:27 +0100

    [TASK] Works with TYPO3 13 too

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '4.1.0',
+    'version' => '4.1.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.5.0-12.5.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [
         ],
